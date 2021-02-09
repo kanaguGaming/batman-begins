d4af64e02647fc574be434fddeb48d5a29fc432a
kanaguGaming <73830217+kanaguGaming@users.noreply.github.com>
2021-02-09 17:29:42 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,84 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+var thunder, thunder1,thunder2,thunder3,thunder4;
+
+//physics engine
+var engine, world;
+var drops = [];
+var rand;
+
+var maxDrops=100;
+
+var thunderCreatedFrame=0;
+
+function preload(){
+    //loading the images
+    thunder1 = loadImage("thunderbolt/1.png");
+    thunder2 = loadImage("thunderbolt/2.png");
+    thunder3 = loadImage("thunderbolt/3.png");
+    thunder4 = loadImage("thunderbolt/4.png");
+}
+
+function setup(){
+    //adding the physics engine to the world
+    engine = Engine.create();
+    world = engine.world;
+
+    //creating canvas
+    createCanvas(400,700);
+    //creating umbrella
+    umbrella = new Umbrella(200,500);
+
+    //creating drops
+    if(frameCount % 150 === 0){
+
+        for(var i=0; i<maxDrops; i++){
+            drops.push(new createDrop(random(0,400), random(0,400)));
+        }
+
+    }
+    
+}
+
+function draw(){
+    //updating the engine
+    Engine.update(engine);
+    background(0); 
+
+    //creating thunder
+    rand = Math.round(random(1,4));
+    if(frameCount%80===0){
+        thunderCreatedFrame=frameCount;
+        thunder = createSprite(random(10,370), random(10,30), 10, 10);
+        switch(rand){
+            case 1: thunder.addImage(thunder1);
+            break;
+            case 2: thunder.addImage(thunder2);
+            break; 
+            case 3: thunder.addImage(thunder3);
+            break;
+            case 4: thunder.addImage(thunder4);
+            break;
+            default: break;
+        }
+        thunder.scale = random(0.3,0.6)
+    }
+
+    if(thunderCreatedFrame + 10 ===frameCount && thunder){
+        thunder.destroy();
+    }
+
+    //displaying umbrella
+    umbrella.display();
+
+    //displaying rain drops
+    for(var i = 0; i<maxDrops; i++){
+        drops[i].showDrop();
+        drops[i].updateY()
+        
+    }
+
+    drawSprites();
+}   
+
